587d19467b08074da875071777e21312c1ac6304
Maxim Esakov <esakovm@dtechs.io>
2026-02-07 22:35:22 +0300

    setup jwt & add register method

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { AuthInput } from './auth.input';
+import { AuthResponse } from './auth.interface';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private auth: AuthService) {}
+
+  /* TODO: Captcha */
+  @Mutation(() => AuthResponse)
+  async register(@Args('data') input: AuthInput) {
+    /* TODO: Add cookie */
+    return this.auth.register(input);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthInput } from './auth.input';
+import { hash } from 'argon2';
+import { IAuthTokenData } from './auth.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private config: ConfigService,
+    private jwt: JwtService,
+  ) {}
+
+  private EXPIRE_DAY_REFRESH_TOKEN = 3;
+  REFRESH_TOKEN_NAME = 'refreshToken';
+
+  async register(input: AuthInput) {
+    try {
+      const email = input.email.toLocaleLowerCase();
+      const existingUser = await this.prisma.user.findFirst({
+        where: {
+          email: {
+            equals: email,
+            mode: 'insensitive',
+          },
+        },
+      });
+
+      if (existingUser) {
+        throw new BadRequestException('User with this email already exist');
+      }
+
+      /* TODO: Move to user service */
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          password: await hash(input.password),
+        },
+      });
+
+      const tokens = this.generateTokens({ id: user.id, role: user.role });
+
+      return { user, ...tokens };
+    } catch (error) {
+      throw new BadRequestException(`Registration failed: ${error}`);
+    }
+  }
+
+  private generateTokens(data: IAuthTokenData) {
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = this.jwt.sign(
+      { id: data.id },
+      {
+        expiresIn: `${this.EXPIRE_DAY_REFRESH_TOKEN}d`,
+      },
+    );
+
+    return { accessToken, refreshToken };
+  }
+}
